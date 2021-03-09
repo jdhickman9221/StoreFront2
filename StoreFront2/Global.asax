70749f18df0edccd7296b0fe4481f9f91d04b6c1
Jordan Hickman <jdhickman816@outlook.com>
2021-03-08 21:49:35 -0600

    Add project files.

---
+++ StoreFront2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StoreFront2.MvcApplication" Language="C#" %>
---
+++ StoreFront2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StoreFront2.Startup))]
+namespace StoreFront2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
